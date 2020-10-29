f34c1107b1ce85703ae20874f899a58af6e08659
samroon2 <sam.roon2454@gmail.com>
2020-10-29 19:46:30 -0400

    Updated setup.py to include pickle.

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ import distutils.command.install as dist_inst
 with open("README.md") as readme:
     long_description = readme.read()
 
+PACKAGE_DATA = {"": ["*.csv", "*.pickle"]}
+
 setup(
     name="ios_data_client",
     version="0.0.1",
     description="Client for getting app data found on the ios app store.",
     packages=find_packages(),
+    package_data=PACKAGE_DATA,
     python_requires=">=3.6.1",
     install_requirements=[
         "sumy>=0.7.0",
