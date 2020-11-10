58df9336a77035995c0ad842c642c2eba3d7f6a9
samroon2 <sam.roon2454@gmail.com>
2020-11-09 22:47:56 -0800

    ...

--- ios_data_client/ios_data_client.py
+++ ios_data_client/ios_data_client.py
@@ -1,6 +1,7 @@
 from .store_countries import CountryCodes
 from .store_data import StoreAppData
 from .store_info import GetStoreInfo
+from .store_reviews import AppReviews
 from .store import Store
 
 class IosDataClient:
@@ -8,6 +9,7 @@ class IosDataClient:
     def __init__(self, **kwargs):
         self.countries = CountryCodes()
         self.store = Store(**kwargs)
+        self.reviews = AppReviews(**kwargs)
 
 
 if __name__ == '__main__':
