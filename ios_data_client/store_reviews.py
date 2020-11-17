5f8d23cbfa3255a5bdcb5dcba0bf6cbc28775857
samroon2 <sam.roon2454@gmail.com>
2020-11-17 11:08:58 -0800

    Added Travis.

--- ios_data_client/store_reviews.py
+++ ios_data_client/store_reviews.py
@@ -29,7 +29,9 @@ class AppReviews:
         self.countrycodealpha = self.countries.codes[self.country]['alpha_2']
         self.headers = {"X-Apple-Store-Front":f'{self.countrycode}',
                         "User-Agent": 'iTunes/12.4.1 (Windows; Microsoft Windows 10.0 x64 Business Edition (Build 9200); x64) AppleWebKit/7601.6016.1000.1'}
+        self.headers_2 = {"User-Agent": 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36'}
         self.sort_orders = {'most recent': 0,'most helpful': 1, 'most favorable' : 2, 'most critical':3}
+        self.tqs = None
 
     def write_to_json(self, file: str, data: dict):
         '''
@@ -42,7 +44,7 @@ class AppReviews:
         with open(f'{file}.json', 'w') as outfile:
             json.dump(data, outfile)
 
-    def get_auth_reviews(self, appid: str, token: str, offset: int):
+    def get_auth_reviews(self, appid: str, token: str, offset: int, alt_headers=False):
         '''
 		Method to make http request to get reviews.
 		:param appid: ID of the app.
@@ -54,10 +56,9 @@ class AppReviews:
 		'''
 		# TODO: language param.
         a = f'https://amp-api.apps.apple.com/v1/catalog/{self.countrycodealpha}/apps/{appid}/reviews?l=en-US&offset={offset}&platform=web&additionalPlatforms=appletv%2Cipad%2Ciphone%2Cmac'
-        # headers = {"User-Agent": 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36'
-        #           ,'authorization': f'Bearer {token}'}
         self.headers['authorization'] = f'Bearer {token}'
-        res = requests.get(a, headers=self.headers)
+        self.headers_2['authorization'] = f'Bearer {token}'
+        res = requests.get(a, headers=self.headers if not alt_headers else self.headers_2)
         return res
 
     def get_all_auth_revs(self, appid: str, token: str, start=0, limit=100):
@@ -88,7 +89,7 @@ class AppReviews:
             else:
                 retry += 1
         
-    def get_all_auth_revs_batch(self, apps: list, token: str, limit=100):
+    def get_all_auth_revs_batch(self, apps: list, token: str, limit=100, alt_headers=False, tqdm_disable=True):
         '''	
 		Method to get reviews for a single app.
 		:param appid: ID of the app.
@@ -99,37 +100,45 @@ class AppReviews:
 		:type limit: int
 		'''
         heap = []
-        tqs = {}
-        for app in apps:
-            tqs[app] = tqdm(total=limit, unit='Reviews')
+        if not self.tqs:
+            self.tqs = {i:tqdm(total=limit, unit='Reviews', disable=tqdm_disable) for i in range(len(apps))}
+        # tqd = tqdm(total=limit*len(apps), unit=' Reviews')
+        tqdm._instances.clear()
+        for i, app in enumerate(apps):
             os.makedirs(f'{str(app)}/reviews') if app not in os.listdir('.') else None
-            heap.append((0, 0, 0, app))
+            heap.append((0, 0, 0, app, i))
         heapq.heapify(heap)
         reqs = 0
         history = collections.deque([])
         while heap:
-            last_ran, offset, retries, app = heapq.heappop(heap)
+            last_ran, offset, retries, app, idx = heapq.heappop(heap)
             if len(history) > 100:
                 history.popleft()
             s_term = list(history)[-10:].count(429)/10
             l_term = history.count(429)/100
             if offset <= limit:
-                if s_term > 0.75:
-                    time.sleep(s_term*10)
                 if reqs-last_ran < 2:
-                    time.sleep(10*l_term)
-                d = self.get_auth_reviews(app, token, offset)
+                    time.sleep(1)
+                try:
+                    d = self.get_auth_reviews(app, token, offset, alt_headers=alt_headers)
+                except Exception as e:
+                    print(e)
+                    if retries < 20:
+                        alt_headers = not alt_headers
+                        heapq.heappush(heap, (reqs+20, offset, retries + 1, app, idx))
+                        reqs += 1
+                    continue
                 if d.status_code == 200:
                     self.write_to_json(f'{app}/reviews/reviews_page_{offset}', d.json())
-                    heapq.heappush(heap, (reqs, offset + 10, 0, app))
+                    heapq.heappush(heap, (reqs, offset + 10, 0, app, idx))
                     reqs += 1
-                    tqs[app].update(10)
+                    self.tqs[idx].update(10)
                     history.append(200)
                 elif d.status_code == 429:
                     if retries < 10:
-                        heapq.heappush(heap, (reqs+10, offset, retries + 1, app))
+                        heapq.heappush(heap, (reqs+10, offset, retries + 1, app, idx))
                         reqs += 1
                         history.append(429)
                 elif retries < 3:
-                    heapq.heappush(heap, (reqs, offset, retries + 1, app))
+                    heapq.heappush(heap, (reqs, offset, retries + 1, app, idx))
                     reqs += 1
